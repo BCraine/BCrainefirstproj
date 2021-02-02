dee6ecc159fb23dff08c0e4a05d4c09a90353067
Brandon Craine <brandoncraine@Brandons-MBP.fios-router.home>
2021-02-02 14:12:12 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+import requests
+import secrets
+
+
+def get_data(url: str):
+    all_data = [] #this will hold the return value
+    full_url = f"{url}&api_key={secrets.api_key}&page=3"
+    response = requests.get(full_url)
+    if response.status_code != 200:
+        print(response.text)
+        return []
+    json_data = response.json()
+    results = json_data['results']
+    all_data.extend(results)
+    return all_data
+
+
+def main():
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name"
+    all_data = get_data(url)
+    for item in all_data:
+        print(item)
+
+if __name__ == '__main__':
+    main()
