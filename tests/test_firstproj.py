c0872fef8ceac0e1918cb3c444a078cbb60c80da
BCraine <brandoncraine193@gmail.com>
2021-02-16 13:18:38 -0500

    first test done

--- tests/test_firstproj.py
+++ tests/test_firstproj.py
@@ -3,7 +3,16 @@ import pytest
 
 
 def test_get_data():
-    pass
+    result = main.get_data(url="https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,"
+                               "3&fields=id,school.state,school.city,school.name,2018.student.size,2016.repayment.3_yr_repayment.overall,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2017.student.size")
+    count = 0
+    for element in result:
+        count +=1
+        return count
+
+    assert count > 1000
+
+
 
 def test_database():
     pass
