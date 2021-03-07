bef38b66ca09beaa3c32c87c082567b6b348903e
BCraine <brandoncraine193@gmail.com>
2021-03-07 14:47:38 -0500

    fixing more linter bugs

--- GuiWindow.py
+++ GuiWindow.py
@@ -1,8 +1,8 @@
-from PySide6.QtWidgets import QWidget, QPushButton, QListWidget, QApplication, QListWidgetItem
+from PySide6 import QtWidgets
 from typing import List, Dict
 
 
-class BCraineGuiWindow(QWidget):
+class BCraineGuiWindow(QtWidgets.QWidget):
     def __init__(self, data_to_show):
         super().__init__()
         self.data = data_to_show
@@ -11,13 +11,13 @@ class BCraineGuiWindow(QWidget):
 
     def setup_window(self):
         self.setWindowTitle("Sprint 4 Gui")
-        display_list = QListWidget(self)
+        display_list = QtWidgets.QListWidget(self)
         self.list_control = display_list
         self.put_data_in_list(self.data)
         display_list.resize(400, 350)
         self.setGeometry(100, 100, 400, 500)
-        quit_button = QPushButton("Quit Now", self)
-        quit_button.clicked.connect(QApplication.instance().quit)
+        quit_button = QtWidgets.QPushButton("Quit Now", self)
+        quit_button.clicked.connect(QtWidgets.QApplication.instance().quit)
         quit_button.resize(quit_button.sizeHint())
         quit_button.move(150, 400)
         self.show()
@@ -27,5 +27,5 @@ class BCraineGuiWindow(QWidget):
             display_text = f"{item['state']}\t\t{item['occ_group']}\t\t{item['major_title']}" \
                            f"\t\t{item['total_employment']}\t\t{item['percentile_25_salary']}" \
                            f"\t\t{item['occupation_code']}"
-            list_item = QListWidgetItem(display_text, listview=self.list_control)
+            list_item = QtWidgets.QListWidgetItem(display_text, listview=self.list_control)
             list_item
